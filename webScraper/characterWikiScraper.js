46ac979cef91843d35b33e74aa365284576c6acd
Gerudhoh <hohenade@uoguelph.ca>
2020-10-02 10:31:35 -0400

    Update comments for clarity

--- webScraper/characterWikiScraper.js
+++ webScraper/characterWikiScraper.js
@@ -15,14 +15,14 @@ async function scrapeWeb(character) {
 	// Build URL
 	let url = wiki + character + game;
 
-	got(url).then(response => {
+	got(url).then(response => {								// Scrape the webpage indicated at the url
 		const $ = cheerio.load(response.body);				// Loads HTML from the url
 		text =  $('.mw-parser-output').text();  			// Parses the text content of a particular div
 		let targetSectionBeginIndex = text.search(targetSection) + regexLen; // Finds the start of the "In competitive play section"
 
 		let targetSectionEndIndex = 0;
 		let nextIndexIsEndOfTargetSection = false;
-		  // Loop through all the sections of the wiki, as denoted in text by the string '[edit]'
+		  // Loop through all the sections of the wiki, as denoted in text by the string '[edit]' in the header
 		  while (match = sectionDefinitionRegex.exec(text)) {
 			  if(nextIndexIsEndOfTargetSection) {
 				  targetSectionEndIndex = match.index;
