d3594f20dfb0d2dc7d6a786e3c5c0a94c6007252
Gerudhoh <hohenade@uoguelph.ca>
2020-10-06 15:42:35 -0400

    changed regexes to simplify lookup

--- webScraper/characterWikiScraper.js
+++ webScraper/characterWikiScraper.js
@@ -4,10 +4,8 @@ var cheerio = require('cheerio');
 const wiki = 'https://www.ssbwiki.com/';
 const game = '_(SSBU)';
 
-const targetSectionHeader = new RegExp(/play\[edit\]/g);
-const regexLen = 10;
-
-const sectionDefinitionRegex = new RegExp(/.*\[edit\]/g);
+const targetSectionHeader = new RegExp(/^In competitive play/gm);
+const sectionThatFollowsTarget = new RegExp(/^Notable players/gm);
 
 async function scrapeWeb(character) {
 	// Build URL
@@ -16,24 +14,11 @@ async function scrapeWeb(character) {
 	got(url).then(response => {								// Scrape the webpage indicated at the url
 		const $ = cheerio.load(response.body);					// Loads HTML from the url
 		let wikiPageText =  $('.mw-parser-output').text();  	// Parses the text content of a particular div
-		let targetSectionBeginIndex = wikiPageText.search(targetSectionHeader) + regexLen; // Finds the start of the "In competitive play section"
-
-		let targetSectionEndIndex = 0;
-		let nextIndexIsEndOfTargetSection = false;
-		  // Loop through all the sections of the wiki, as denoted in text by the string '[edit]' in the header
-		  while (match = sectionDefinitionRegex.exec(wikiPageText)) {
-			  if(nextIndexIsEndOfTargetSection) {
-				  targetSectionEndIndex = match.index;
-				  break;
-			  }
-	
-			  if(sectionDefinitionRegex.lastIndex === targetSectionBeginIndex) {
-				nextIndexIsEndOfTargetSection = true;
-			  }
-		  }
-		  let competitiveInformation = wikiPageText.substring(targetSectionBeginIndex, targetSectionEndIndex);
-		  console.log(competitiveInformation);
-		  return competitiveInformation;
+		let targetSectionBeginIndex = wikiPageText.search(targetSectionHeader); // Finds the start of the "In competitive play section"
+		let targetSectionEndIndex = wikiPageText.search(sectionThatFollowsTarget);
+		let competitiveInformation = wikiPageText.substring(targetSectionBeginIndex, targetSectionEndIndex);
+		console.log(competitiveInformation);
+		return competitiveInformation;
 	}).catch(err => {
 		console.log(err);
 	});
