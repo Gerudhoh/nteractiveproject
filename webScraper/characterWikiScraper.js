7dc6e90265318f96112a7b21f0012878d94996e3
Gerudhoh <hohenade@uoguelph.ca>
2020-10-08 13:51:42 -0400

    fixed async problem!

--- webScraper/characterWikiScraper.js
+++ webScraper/characterWikiScraper.js
@@ -12,22 +12,24 @@ regexesMap.set('In competitive play', {
 
 
 async function scrapeWeb(character, targetText) {
+	let competitiveInformation = "";
 	// Build URL
 	let url = wiki + character + game;
 
-	got(url).then(response => {									// Scrape the webpage indicated at the url
+	await got(url).then(response => {									// Scrape the webpage indicated at the url
 		const $ = cheerio.load(response.body);					// Loads HTML from the url
 		let wikiPageText =  $('.mw-parser-output').text();  	// Parses the text content of a particular div, based on its css class
 		// console.log(wikiPageText);							// Shows all the text we scraped. Good for building our regexes.
 		let regexes = regexesMap.get(targetText);
 		let targetSectionStartIndex = wikiPageText.search(regexes.startSection);
 		let targetSectionEndIndex = wikiPageText.search(regexes.endSection);
-		let competitiveInformation = wikiPageText.substring(targetSectionStartIndex, targetSectionEndIndex);
-		console.log(competitiveInformation);
-		return competitiveInformation;
+		competitiveInformation = wikiPageText.substring(targetSectionStartIndex, targetSectionEndIndex);
 	}).catch(err => {
 		console.log(err);
 	});
+	return competitiveInformation;
 }
 
-scrapeWeb("Peach", 'In competitive play');
+scrapeWeb("Peach", 'In competitive play').then(function(result) {
+	console.log(result);
+ })
