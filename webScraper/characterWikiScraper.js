f5123b2833716be9eef02745b7d7efe9a6746670
Gerudhoh <hohenade@uoguelph.ca>
2020-10-27 13:00:09 -0400

    Refactoring the regex map into its own class so that it may be used in a wider variety of ways and conform to SRP

--- webScraper/characterWikiScraper.js
+++ webScraper/characterWikiScraper.js
@@ -1,14 +1,9 @@
 const got = require('got');
 const cheerio = require('cheerio');
+const regexMap = require("./regexMap");
 
 const wiki = 'https://www.ssbwiki.com/';
 const game = '_(SSBU)';
-
-const regexesMap = new Map;
-regexesMap.set('In competitive play', {
-  startSection: new RegExp(/^In competitive play/gm),
-  endSection: new RegExp(/^Notable players/gm),
-});
 
 /**
  *  Scrapes the smash wiki
@@ -24,7 +19,7 @@ async function scrapeWeb(character, targetText) {
     const $ = cheerio.load(response.body); // Loads HTML from the url
     const wikiPageText = $('.mw-parser-output').text(); // Parses the text content of a particular div, based on its css class
     // console.log(wikiPageText);                       // Shows all the text we scraped. Good for building our regexes.
-    const regexes = regexesMap.get(targetText);
+    const regexes = regexMap.getRegexes().get(targetText);
     const targetStartIndex = wikiPageText.search(regexes.startSection);
     const targetEndIndex = wikiPageText.search(regexes.endSection);
     competitiveInfo = wikiPageText.substring(targetStartIndex, targetEndIndex);
---
+++ webScraper/regexMap.js
@@ -0,0 +1,11 @@
+const regexesMap = new Map;
+regexesMap.set('In competitive play', {
+  startSection: new RegExp(/^In competitive play/gm),
+  endSection: new RegExp(/^Notable players/gm),
+});
+
+module.exports = {
+  getRegexes: function() {
+    return regexesMap;
+  }
+}
