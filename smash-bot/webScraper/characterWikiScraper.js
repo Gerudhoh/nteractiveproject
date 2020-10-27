be7b1b7f6ee8fdce84fc2b8e824850d9ffff7a6b
Gerudhoh <hohenade@uoguelph.ca>
2020-10-27 15:21:39 -0400

    Revert "Merge branch 'master' of https://git.socs.uoguelph.ca/3760f20/nteractive/nteractiveproject"
    This reverts commit 2f0a225bd1b95c007283bd71ba242492980a3dbe, reversing
    changes made to 12e515f4203b6e49fe3c4c713560de882acc7a48.

--- smash-bot/webScraper/characterWikiScraper.js
+++ smash-bot/webScraper/characterWikiScraper.js
@@ -1,9 +1,14 @@
 const got = require('got');
 const cheerio = require('cheerio');
-const regexMap = require('./regexMap');
 
 const wiki = 'https://www.ssbwiki.com/';
 const game = '_(SSBU)';
+
+const regexesMap = new Map;
+regexesMap.set('In competitive play', {
+  startSection: new RegExp(/^In competitive play/gm),
+  endSection: new RegExp(/^Notable players/gm),
+});
 
 /**
  *  Scrapes the smash wiki
@@ -19,7 +24,7 @@ async function scrapeWeb(character, targetText) {
     const $ = cheerio.load(response.body); // Loads HTML from the url
     const wikiPageText = $('.mw-parser-output').text(); // Parses the text content of a particular div, based on its css class
     // console.log(wikiPageText);                       // Shows all the text we scraped. Good for building our regexes.
-    const regexes = regexMap.getRegexes().get(targetText);
+    const regexes = regexesMap.get(targetText);
     const targetStartIndex = wikiPageText.search(regexes.startSection);
     const targetEndIndex = wikiPageText.search(regexes.endSection);
     competitiveInfo = wikiPageText.substring(targetStartIndex, targetEndIndex);
