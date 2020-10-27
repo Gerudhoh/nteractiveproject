5b0497aaa952fdcf1235ea96e2ee6e73c382f2de
Gerudhoh <hohenade@uoguelph.ca>
2020-10-27 16:22:49 -0400

    fixing for linting

--- smash-bot/bot.js
+++ smash-bot/bot.js
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-const { ActivityHandler, MessageFactory } = require('botbuilder');
+const {ActivityHandler, MessageFactory} = require('botbuilder');
 
 // nteractive code:
 const characterInfo = require('./webScraper/characterWikiScraper');
@@ -10,41 +10,41 @@ const characterInfo = require('./webScraper/characterWikiScraper');
  *  The bot object.
  */
 class EchoBot extends ActivityHandler {
-    /**
+  /**
      *  Setup calls.
      */
-    constructor() {
-        super();
-        // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
-        this.onMessage(async (context, next) => {
-            let replyText = "";
-            const input = context.activity.text.split(",");
-            if(input.length == 2){
-    	        replyText = await characterInfo.scrapeWeb(input[0], input[1]);
-            } 
-            else {
-                replyText = `Oops! I don't quite understand ${context.activity.text}.\n`;
-                replyText += "Be sure to ask me questions in the format Character Name, Information i.e. Peach, In competitive play";
-            }
-            await context.sendActivity(MessageFactory.text(replyText, replyText));
-            // By calling next() you ensure that the next BotHandler is run.
-            await next();
-        });
+  constructor() {
+    super();
+    // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
+    this.onMessage(async (context, next) => {
+      let replyText = '';
+      const input = context.activity.text.split(',');
+      if (input.length == 2) {
+        replyText = await characterInfo.scrapeWeb(input[0], input[1]);
+      } else {
+        replyText = `Oops! I don't quite understand ${context.activity.text}.\n`;
+        replyText += 'Be sure to ask me questions in the format:\n';
+        replyText += 'Character Name, Information i.e. Peach, In competitive play';
+      }
+      await context.sendActivity(MessageFactory.text(replyText, replyText));
+      // By calling next() you ensure that the next BotHandler is run.
+      await next();
+    });
 
-        this.onMembersAdded(async (context, next) => {
-            const membersAdded = context.activity.membersAdded;
-            let welcomeText = "Hello and welcome! I'm the Super Smash Bros Bot.\n";
-            welcomeText += "Ask me a question like this: \n";
-            welcomeText += "Character Name, Information  i.e. Peach, In competitive play";
-            for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
-                if (membersAdded[cnt].id !== context.activity.recipient.id) {
-                    await context.sendActivity(MessageFactory.text(welcomeText, welcomeText));
-                }
-            }
-            // By calling next() you ensure that the next BotHandler is run.
-            await next();
-        });
-    }
+    this.onMembersAdded(async (context, next) => {
+      const membersAdded = context.activity.membersAdded;
+      let welcomeText = 'Hello and welcome! I\'m the Super Smash Bros Bot.\n';
+      welcomeText += 'Ask me a question like this: \n';
+      welcomeText += 'Character Name, Information  i.e. Peach, In competitive play';
+      for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
+        if (membersAdded[cnt].id !== context.activity.recipient.id) {
+          await context.sendActivity(MessageFactory.text(welcomeText, welcomeText));
+        }
+      }
+      // By calling next() you ensure that the next BotHandler is run.
+      await next();
+    });
+  }
 }
 
 module.exports.EchoBot = EchoBot;
