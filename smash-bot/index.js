ed7eb8ef790f71376b31f82ab6a06d42c1cb8e7a
sysadmin <sysadmin@guest-216.socs.uoguelph.ca>
2020-11-19 15:25:45 -0500

    Removed unnecessary require statements

--- smash-bot/index.js
+++ smash-bot/index.js
@@ -4,8 +4,6 @@
 const path = require('path');
 const fs = require('fs');
 const dotenv = require('dotenv');
-const express = require('express');
-const app = new express();
 
 // Import required bot configuration.
 const ENV_FILE = path.join(__dirname, '.env');
