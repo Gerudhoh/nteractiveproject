07456b91bda69643ca816b597e8e8625b5ac5708
Christopher Kotsopoulos <kotsopoc@uoguelph.ca>
2020-11-25 15:51:20 -0500

    fixing linter errors for unit test

--- test/viabilityCheckerTest.js
+++ test/viabilityCheckerTest.js
@@ -1,5 +1,4 @@
 const expect = require('chai').expect;
-const fs = require('fs');
 
 const viabilityChecker = require('../smash-bot/csvScraper/viabilityChecker');
 
@@ -10,15 +9,15 @@ describe('Viability Checker tests', function() {
   describe('Pull Competitive Information', function() {
     it('Pulls correct competitve viability between Peach vs Mario', function() {
       viabilityChecker.checkViability('Peach-Mario')
-        .then(function(viability) {
-          expect(viability.trim()).to.equal(peachVSMarioViability);
-        });
+          .then(function(viability) {
+            expect(viability.trim()).to.equal(peachVSMarioViability);
+          });
     });
     it('Gets an error for finding the matchup of unknown characters', function() {
       viabilityChecker.checkViability('fake-character')
-        .then(function(viability) {
-          expect(viability.trim()).to.equal(errorViability);
-        });
-      });
+          .then(function(viability) {
+            expect(viability.trim()).to.equal(errorViability);
+          });
+    });
   });
 });
