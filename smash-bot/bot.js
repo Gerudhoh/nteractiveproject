0eede09dea56adf424081696733ea4de3abd068f
Gerudhoh <hohenade@uoguelph.ca>
2020-11-27 12:27:25 -0500

    trying to implement the strategy

--- smash-bot/bot.js
+++ smash-bot/bot.js
@@ -7,6 +7,12 @@ const {ActivityHandler, MessageFactory} = require('botbuilder');
 const characterInfo = require('./webScraper/characterWikiScraper');
 const checkViability = require('./csvScraper/viabilityChecker');
 
+let Strategies = new Map();
+Strategies.set("matchup", { strategy: new Strategy("matchup", checkViability.checkViability});
+Strategies.set("in competitive play", { strategy: new Strategy("in competitive play", characterInfo.scrapeWeb)});
+Strategies.set("updates", { strategy: new Strategy("updates", characterInfo.scrapeWeb)});
+Strategies.set("moveset", { strategy: new Strategy("moveset", characterInfo.scrapeWeb)});
+
 /**
  *  The bot object.
  */
@@ -20,19 +26,23 @@ class EchoBot extends ActivityHandler {
     this.onMessage(async (context, next) => {
       let replyText = '';
       const input = context.activity.text.split(',');
-      if (input.length == 2) {
-        try {
-          replyText = await characterInfo.scrapeWeb(context.activity.text);
-        } catch (error) {
-          replyText = `Oops! Something went wrong. Please try again :)`;
-        }
-      } else if (input.length == 3) {
-        replyText = await checkViability.checkViability(context.activity.text);
-      } else {
-        replyText = `Oops! I don't quite understand ${context.activity.text}.\n`;
-        replyText += 'Be sure to ask me questions in the format:\n';
-        replyText += 'Command, Character Name i.e.: In competitive play, Peach or update:3.0.0, Kirby';
-      }
+      let fun = Strategies["matchup"];
+
+      replyText = await fun(context.activity.text);
+      console.log(replyText);
+      // if (input.length == 2) {
+      //   try {
+      //     replyText = await characterInfo.scrapeWeb(context.activity.text);
+      //   } catch (error) {
+      //     replyText = `Oops! Something went wrong. Please try again :)`;
+      //   }
+      // } else if (input.length == 3) {
+      //   replyText = await checkViability.checkViability(context.activity.text);
+      // } else {
+      //   replyText = `Oops! I don't quite understand ${context.activity.text}.\n`;
+      //   replyText += 'Be sure to ask me questions in the format:\n';
+      //   replyText += 'Command, Character Name i.e.: In competitive play, Peach or update:3.0.0, Kirby';
+      // }
       await context.sendActivity(MessageFactory.text(replyText, replyText));
       // By calling next() you ensure that the next BotHandler is run.
       await next();
