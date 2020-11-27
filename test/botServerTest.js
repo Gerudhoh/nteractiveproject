009ca27de6c05e3470ff4a6711c5dd454079b90b
Gerudhoh <hohenade@uoguelph.ca>
2020-11-27 15:42:53 -0500

    rename bot test

---
+++ test/botServerTest.js
@@ -0,0 +1,13 @@
+const expect = require('chai').expect;
+const request = require('request');
+
+describe('Test bot connection', function() {
+  const url = 'https://https://nteractive.socs.uoguelph.ca/';
+
+  xit('returns status 200', function(done) {
+    request(url, function(error, response, body) {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+});
